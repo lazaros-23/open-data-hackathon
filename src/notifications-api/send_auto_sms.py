4e4452ec4beb692fce5a87844d8d6cb23c275117
lazaros-23 <52760612+lazaros-23@users.noreply.github.com>
2022-11-20 14:19:56 +0200

    Update send_auto_sms.py

--- src/notifications-api/send_auto_sms.py
+++ src/notifications-api/send_auto_sms.py
@@ -1,7 +1,7 @@
 import requests
 resp = requests.post('https://textbelt.com/text', {
   'phone': '+30 6955759077',
-  'message': 'to kinito sou parakolouthite, pouthena den mporeis na pas xoris na to ksero isisisisisissis',
+  'message': 'To αίτημα σου Ικανοποιήθηκε, Βρες την ανάλυση νερού στο παρακάτω pdf',
   'key': 'textbelt',
 })
 print(resp.json())
